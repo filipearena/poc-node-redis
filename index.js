8bcd618a4b5f5b650d2456a8e2ec1cb526274b9b
Arena <filipe.gentil.arena@accenture.com>
2017-02-25 03:26:36 -0300

    index.js fix

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ var path = require('path');
 var express = require('express');
 
 var port = process.env.PORT || '9001';
-var rootPath = path.normalize(__dirname);
-
 var app = express();
 
-app.get('/', function (req, res) {
-    return res.redirect('/');
-});
+app.use('/', express.static(path.resolve(path.join(__dirname, "app"))));
 
 app.listen(port, undefined, function () {
     console.log('Listening on port %d', port);
